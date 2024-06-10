6abd7e61183f3ff1e000c2a4461c02b18b85b408
Rebecca Weiss <rebeccaw00@gmail.com>
2024-06-10 18:38:26 +0200

    code reformatting

--- demonstrator.py
+++ demonstrator.py
@@ -2,6 +2,7 @@ import MDPSolver
 from environment import Environment
 import copy
 
+
 class Demonstrator:
     """
     class to implement the demonstrator that computes its policy for a certain reward using value iteration
@@ -14,19 +15,21 @@ class Demonstrator:
         name of the demonstrator
     T : int
         finite horizon value for the MDP solver (default = 10)
-    
+
     """
-    def __init__(self, env:Environment, demonstrator_name:str, T:int = 10):
+
+    def __init__(self, env: Environment, demonstrator_name: str, T: int = 10):
         self.V = None
         self.pi = None
         self.reward = None
         self.env = env
         self.demonstrator_name = demonstrator_name
         self.solver = MDPSolver.MDPSolverExpectation(T)
-        self.mu_demonstrator = self.get_mu_usingRewardFeatures(self.env, self.env.reward)
-        
+        self.mu_demonstrator = self.get_mu_usingRewardFeatures(
+            self.env, self.env.reward
+        )
 
-    def get_mu_usingRewardFeatures(self, env:Environment, reward):
+    def get_mu_usingRewardFeatures(self, env: Environment, reward):
         """
         computes feature expectation and variance terms for the demonstrator using value iteration and computing the value function based on it
 
@@ -46,10 +49,12 @@ class Demonstrator:
         self.pi = pi_s
 
         _, mu, nu = self.solver.computeFeatureSVF_bellmann_averaged(env, pi_s)
-        return mu[:env.n_features_reward], nu[:env.n_features_reward,:env.n_features_reward]
+        return (
+            mu[: env.n_features_reward],
+            nu[: env.n_features_reward, : env.n_features_reward],
+        )
 
-
-    def draw(self, show =False):
+    def draw(self, show=False):
         """
         draws the policy of the demonstrator as long as it has been computed before, else a warning is thrown
         """
