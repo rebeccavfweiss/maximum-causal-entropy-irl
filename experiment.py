8b4cad61468ed025a82f655a68ba141cd8b0e8bf
Rebecca Weiss <rebeccaw00@gmail.com>
2024-07-02 19:25:23 +0200

    code formatting

--- experiment.py
+++ experiment.py
@@ -6,30 +6,26 @@ import numpy as np
 
 
 def create_env():
-    
-    config_env = {
-                "theta_e": [1.0, 1.0, -2.0],
-                "gamma": 1.0
-                }
-    
+
+    config_env = {"theta_e": [1.0, 1.0, -2.0], "gamma": 1.0}
+
     env = environment.Environment(config_env)
 
     return env
 
+
 def create_config_learner():
-    config_default_learner = {"tol": 0.0005,
-                                "miniter": 5,
-                                "maxiter": 30000
-                            }
+    config_default_learner = {"tol": 0.0005, "miniter": 5, "maxiter": 30000}
 
     return config_default_learner
+
 
 if __name__ == "__main__":
 
     show = False
     store = True
     verbose = False
-    T = 20
+    T = 10
 
     # create the environment
     env = create_env()
@@ -45,26 +41,30 @@ if __name__ == "__main__":
 
     if verbose:
         print("Demonstrator done")
-    
-    reward_demonstrator = np.dot(env.reward, demo.solver.computeFeatureSVF_bellmann(env, demo.pi)[0])
+
+    reward_demonstrator = np.dot(
+        env.reward, demo.solver.computeFeatureSVF_bellmann(env, demo.pi)[0]
+    )
 
     # create agent that uses only expectation matching
-    agent_expectation = agent.Agent(env, demo.mu_demonstrator, config_default_learner, agent_name="Agent Expectation", solver=MDPSolver.MDPSolverExpectation(T))
+    agent_expectation = agent.Agent(
+        env,
+        demo.mu_demonstrator,
+        config_default_learner,
+        agent_name="Agent Expectation",
+        solver=MDPSolver.MDPSolverExpectation(T),
+    )
     iter_expectation, time_expectation = agent_expectation.batch_MCE(verbose=verbose)
     agent_expectation.compute_and_draw(show, store, 2)
-    reward_expectation = np.dot(env.reward, agent_expectation.solver.computeFeatureSVF_bellmann(env, agent_expectation.pi)[0])
+    reward_expectation = np.dot(
+        env.reward,
+        agent_expectation.solver.computeFeatureSVF_bellmann(env, agent_expectation.pi)[
+            0
+        ],
+    )
 
     if verbose:
         print("First agent done")
-
-    # create agent that also matches variances
-    agent_variance = agent.Agent(env, demo.mu_demonstrator, config_default_learner, agent_name="Agent Variance", solver=MDPSolver.MDPSolverVariance(T))
-    iter_variance, time_variance = agent_variance.batch_MCE(verbose=verbose)
-    agent_variance.compute_and_draw(show, store, 4)
-    reward_variance = np.dot(env.reward, agent_variance.solver.computeFeatureSVF_bellmann(env, agent_variance.pi)[0])
-
-    if verbose:
-        print("Second agent done")
 
     print("-- Results --")
 
@@ -74,19 +74,69 @@ if __name__ == "__main__":
     print("")
 
     print("----- Expectation -----")
-    print("reward: ", reward_expectation, " (diff. to demonstrator: ", np.abs(reward_demonstrator-reward_expectation), ")")
+    print(
+        "reward: ",
+        reward_expectation,
+        " (diff. to demonstrator: ",
+        np.abs(reward_demonstrator - reward_expectation),
+        ")",
+    )
     print("theta_e: ", agent_expectation.theta_e)
-    print("policy difference:", sum(np.linalg.norm(demo.pi[i,:,:] - agent_expectation.pi[i,:,:], ord = "fro") for i in range(demo.pi.shape[0])))
+    print(
+        "policy difference:",
+        sum(
+            np.linalg.norm(demo.pi[i, :, :] - agent_expectation.pi[i, :, :], ord="fro")
+            for i in range(demo.pi.shape[0])
+        ),
+    )
     print("iterations used: ", iter_expectation)
-    print("time used (total/ avg. per iteration): ", sum(time_expectation), "/", np.mean(time_expectation))
+    print(
+        "time used (total/ avg. per iteration): ",
+        sum(time_expectation),
+        "/",
+        np.mean(time_expectation),
+    )
     print("")
 
+    # create agent that also matches variances
+    agent_variance = agent.Agent(
+        env,
+        demo.mu_demonstrator,
+        config_default_learner,
+        agent_name="Agent Variance",
+        solver=MDPSolver.MDPSolverVariance(T),
+    )
+    iter_variance, time_variance = agent_variance.batch_MCE(verbose=verbose)
+    agent_variance.compute_and_draw(show, store, 4)
+    reward_variance = np.dot(
+        env.reward,
+        agent_variance.solver.computeFeatureSVF_bellmann(env, agent_variance.pi)[0],
+    )
+
+    if verbose:
+        print("Second agent done")
+
     print("----- Expectation + Variance -----")
-    print("reward: ", reward_variance, " (diff. to demonstrator: ", np.abs(reward_demonstrator-reward_variance), ")")
+    print(
+        "reward: ",
+        reward_variance,
+        " (diff. to demonstrator: ",
+        np.abs(reward_demonstrator - reward_variance),
+        ")",
+    )
     print("theta_e: ", agent_variance.theta_e)
     print("theta_v: ", agent_variance.theta_v)
-    print("policy difference:", sum(np.linalg.norm(demo.pi[i,:,:] - agent_variance.pi[i,:,:], ord ="fro") for i in range(demo.pi.shape[0])))
+    print(
+        "policy difference:",
+        sum(
+            np.linalg.norm(demo.pi[i, :, :] - agent_variance.pi[i, :, :], ord="fro")
+            for i in range(demo.pi.shape[0])
+        ),
+    )
     print("iterations used: ", iter_variance)
-    print("time used (total/ avg. per iteration): ", sum(time_variance), "/", np.mean(time_variance))
-
-    
+    print(
+        "time used (total/ avg. per iteration): ",
+        sum(time_variance),
+        "/",
+        np.mean(time_variance),
+    )
