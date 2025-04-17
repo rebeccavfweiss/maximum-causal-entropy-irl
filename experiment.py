c7d86d96df9fe2ee34ec16bc8a1678e4a67d3fc8
Rebecca Weiss <rebeccaw00@gmail.com>
2025-04-17 12:50:21 +0200

    Update theta for reward computation

--- experiment.py
+++ experiment.py
@@ -22,7 +22,7 @@ def create_simple_env():
 def create_minigrid_env():
 
     config_env = {
-        "theta": np.diag([-1.0, -1.0, 0.0, 10.0, -10.0]),
+        "theta": np.diag([-1.0, -1.0, 0.5, 0.5, 10.0, -10.0]),
         "gamma": 1.0,
         "env_name": "MiniGrid-LavaCrossingS9N1-v0",
         "render_mode": "rgb_array",
