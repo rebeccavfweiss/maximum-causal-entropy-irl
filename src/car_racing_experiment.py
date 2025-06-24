4ae197ff402dca36535c3218fafa5616377b6079
Rebecca Weiss <rebeccaw00@gmail.com>
2025-06-24 16:12:21 +0200

    Change logging to use WandB

--- src/car_racing_experiment.py
+++ src/car_racing_experiment.py
@@ -4,9 +4,8 @@ from environments.car_racing_environment import CarRacingEnvironment
 from MDP_solver_approximation import MDPSolverApproximationExpectation, MDPSolverApproximationVariance
 import numpy as np
 import psutil
-from utils import create_logger, log
 import gc
-
+import wandb
 
 def create_carracing_env(lap_complete_percent:float=0.95, T:int = 1000):
 
@@ -28,15 +27,19 @@ def create_config_learner(n_trajectories:int = 1, maxiter:int=3):
 
     return config_default_learner
 
-def log_memory(stage="", verbose:bool=False):
+def log_memory(stage=""):
     mem = psutil.virtual_memory()
-    log(f"[MEMORY] {stage}: {mem.available / (1024 ** 2):.2f} MB free", verbose)
+    wandb.log({
+        f"memory_free_mb_{stage}": mem.available / (1024 ** 2),
+        f"memory_used_percent_{stage}": mem.percent,
+        f"memory_total_mb_{stage}": mem.total / (1024 ** 2),
+    })
 
 if __name__ == "__main__":
 
     show = False
     store = True
-    verbose = False
+    experiment_name = "car_racing"
 
     maxiter = 30
     n_trajectories = 100
@@ -46,9 +49,23 @@ if __name__ == "__main__":
     lap_percent_complete=0.95
     T = 200
 
-    create_logger("medium_experiment_log")
+    learning_rate = lambda step: max(0.99 ** (step + 1), 0.01)
 
-    log_memory("Start", verbose)
+
+    wandb.init(
+    project="mceirl-car-racing",
+    name=f"{experiment_name}-run",
+    config={
+        "maxiter": maxiter,
+        "n_trajectories": n_trajectories,
+        "sac_timesteps": sac_timesteps,
+        "sac_buffer_size": sac_buffer_size,
+        "lap_percent_complete": lap_percent_complete,
+        "T": T
+    }
+)
+
+    log_memory("start")
 
     # create the environment
     env = create_carracing_env(lap_complete_percent=lap_percent_complete, T=T)
@@ -56,7 +73,7 @@ if __name__ == "__main__":
     # Learner config
     config_default_learner = create_config_learner(n_trajectories, maxiter)
 
-    log_memory("Environment + Config created", verbose)
+    log_memory("env_config_creation")
 
     # create demonstrator
     demo = demonstrator.CarRacingDemonstrator(
@@ -64,24 +81,22 @@ if __name__ == "__main__":
         demonstrator_name="CarRacingDemonstrator",
         T=T,
         n_trajectories=n_trajectories,
-        solver = MDPSolverApproximationExpectation(T,True, sac_timesteps, sac_buffer_size)
+        solver = MDPSolverApproximationExpectation(experiment_name, T,True, sac_timesteps, sac_buffer_size)
     )
 
-    log_memory("Demonstrator created", verbose)
-
-    log("Demonstrator's expected value: {}".format(demo.mu_demonstrator[0]), verbose)
-    log("Demonstrator's variance: {}".format(demo.mu_demonstrator[1]), verbose)
-
+    log_memory("demonstrator_creation")
 
     reward_demonstrator = env.compute_true_reward_for_agent(demo, n_trajectories, T)
 
-    demo.draw(show, store, 0)
+    wandb.log({"demonstrator_expected_value" : demo.mu_demonstrator[0],
+            "demonstrator_variance": demo.mu_demonstrator[1],
+            "demonstrator_reward": reward_demonstrator})
+
+    demo.render(show, store, 0)
 
     # clean up
     del demo.policy
-    log_memory("After demonstrator run + cleanup", verbose)
-
-    log("Demonstrator done", verbose)
+    log_memory("demonstrator_policy_cleanup")
 
     # create agent that uses only expectation matching
     agent_expectation = learner.ApproximateLearner(
@@ -89,27 +104,32 @@ if __name__ == "__main__":
         demo.mu_demonstrator,
         config_default_learner,
         agent_name="AgentExpectation",
-        solver=MDPSolverApproximationExpectation(T, sac_timesteps=sac_timesteps, sac_buffer_size=sac_buffer_size),
+        solver=MDPSolverApproximationExpectation(experiment_name, T, sac_timesteps=sac_timesteps, sac_buffer_size=sac_buffer_size),
+        learning_rate= learning_rate
     )
 
-    iter_expectation, time_expectation = agent_expectation.batch_MCE(verbose=verbose)
+    iter_expectation, time_expectation = agent_expectation.batch_MCE()
     agent_expectation.compute_and_draw(show, store, 2)
     reward_expectation = env.compute_true_reward_for_agent(agent_expectation, n_trajectories, T)
 
-    log_memory("After expectation-only agent", verbose)    
+    log_memory("agent_expectation_finished")   
 
-    log("First agent done", verbose)
+    artifact = wandb.Artifact(f"agent_expectation_model", type="model")
+    artifact.add_file(agent_expectation.solver.model_dir/"best_model.zip")
+    wandb.log_artifact(artifact) 
 
-    log(f"""----- Expectation -----
-    reward: {reward_expectation} (diff. to demonstrator: {np.abs(reward_demonstrator - reward_expectation)})
-    theta_e: {agent_expectation.theta_e if verbose else 'hidden'}
-    iterations used: {iter_expectation}
-    time used (total/ avg. per iteration): {sum(time_expectation)} / {np.mean(time_expectation)}
-    """, verbose)
+
+    wandb.log({
+        "reward_expectation": reward_expectation,
+        "reward_diff_expectation": np.abs(reward_demonstrator - reward_expectation),
+        "iterations_expectation": iter_expectation,
+        "time_total_expectation": sum(time_expectation),
+        "time_avg_per_iter_expectation": np.mean(time_expectation),
+    })
 
     del agent_expectation
     gc.collect()
-    log_memory("After expectation policy clean up", verbose)
+    log_memory("agent_expectation_policy_cleanup")
 
     # create agent that also matches variances
     agent_variance = learner.ApproximateLearner(
@@ -117,35 +137,31 @@ if __name__ == "__main__":
         demo.mu_demonstrator,
         config_default_learner,
         agent_name="AgentVariance",
-        solver=MDPSolverApproximationVariance(T, sac_timesteps=sac_timesteps, sac_buffer_size=sac_buffer_size),
+        solver=MDPSolverApproximationVariance(experiment_name,T, sac_timesteps=sac_timesteps, sac_buffer_size=sac_buffer_size),
+        learning_rate=learning_rate
     )
 
-    iter_variance, time_variance = agent_variance.batch_MCE(verbose=verbose)
+    iter_variance, time_variance = agent_variance.batch_MCE()
     agent_variance.compute_and_draw(show, store, 4)
     reward_variance = env.compute_true_reward_for_agent(agent_variance, n_trajectories, T)
 
-    log_memory("After expectation + variance agent", verbose)
+    log_memory("agent_variance_finished")
+
+    artifact = wandb.Artifact(f"agent_variance_model", type="model")
+    artifact.add_file(agent_variance.solver.model_dir/ "best_model.zip")
+    wandb.log_artifact(artifact) 
 
     # Free up memory
     del agent_variance.policy
-    log_memory("After variance policy clean up", verbose)
+    log_memory("agent_expectation_policy_cleanup")
 
-    if verbose:
-        log("Second agent done", verbose)
+    wandb.log({
+        "reward_variance": reward_variance,
+        "reward_diff_variance": np.abs(reward_demonstrator - reward_variance),
+        "iterations_variance": iter_variance,
+        "time_total_variance": sum(time_variance),
+        "time_avg_per_iter_variance": np.mean(time_variance),
+    })
 
-    log("----- Expectation + Variance -----", verbose)
-    log(
-        f"reward: {reward_variance} "
-        f"(diff. to demonstrator: {np.abs(reward_demonstrator - reward_variance)})",
-        verbose
-    )
+    wandb.finish()
 
-    log(f"theta_e: {agent_variance.theta_e}", verbose)
-    log(f"theta_v: {agent_variance.theta_v}", verbose)
-
-    log(f"iterations used: {iter_variance}", verbose)
-    log(
-        f"time used (total/ avg. per iteration): "
-        f"{sum(time_variance)} / {np.mean(time_variance)}",
-        verbose
-    )
