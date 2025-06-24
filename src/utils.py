e507014e03af4045ccac1d5e7b5202343a050c37
Rebecca Weiss <rebeccaw00@gmail.com>
2025-06-24 16:11:54 +0200

    Remove redundant logging functionalities, add new callback class for tracking runtimes

--- src/utils.py
+++ src/utils.py
@@ -1,42 +1,11 @@
-import logging
+import os
+from stable_baselines3.common.callbacks import EvalCallback
+import time
+import numpy as np
 import os
 
-def create_logger(filename:str="experiment_log") -> None:
-    """
-    Utility function to create a logger that logs into a given file
-    
-    Parameters
-    ----------
-    filename : str
-        log file name
-    """
-    
-    # Create logs directory if needed
-    os.makedirs("logs", exist_ok=True)
-    # Configure logging
-    logging.basicConfig(
-        filename=f"logs/{filename}.txt",
-        filemode="w",  # Overwrite on each run. Use "a" to append.
-        format="%(asctime)s - %(message)s",
-        level=logging.INFO
-    )
 
-def log(msg:str, verbose:bool=False) -> None:
-    """
-    Logging function that can also print to the console if desired
-
-    Parameters
-    ----------
-    msg : str
-        message to log
-    verbose : bool
-        whether message should be printed to the console
-    """
-    logging.info(msg)
-    if verbose:
-        print(msg)
-
-def is_truncated_from_infos(infos:list[dict])->bool:
+def is_truncated_from_infos(infos: list[dict]) -> bool:
     """
     Given a list of info dicts (one per env), return True if any of the dicts
     contain a key with 'truncated' in its name and that value is True.
@@ -52,6 +21,34 @@ def is_truncated_from_infos(infos:list[dict])->bool:
         True if any truncated-related key is True in any info dict.
     """
     for info in infos:
-        if any(key.lower().find('truncated') != -1 and info[key] for key in info):
+        if any(key.lower().find("truncated") != -1 and info[key] for key in info):
             return True
     return False
+
+
+class TimedEvalCallback(EvalCallback):
+    """
+    Custom callback function to also track training time expenses
+    """
+
+    def __init__(self, *args, time_log_path=None, **kwargs):
+
+        super().__init__(*args, **kwargs)
+        self.eval_times = []
+        self.time_log_path = time_log_path or os.path.join(
+            self.log_path, "evaluation_times.npy"
+        )
+
+        os.makedirs(os.path.dirname(self.time_log_path), exist_ok=True)
+
+    def _on_step(self) -> bool:
+        start_time = time.time()
+        result = super()._on_step()
+        end_time = time.time()
+
+        if self.eval_freq > 0 and self.n_calls % self.eval_freq == 0:
+            duration = end_time - start_time
+            self.eval_times.append(duration)
+            np.save(self.time_log_path, np.array(self.eval_times))
+
+        return result
