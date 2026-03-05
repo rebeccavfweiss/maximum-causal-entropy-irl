8d8de15fa94aa34da9252b8edb2895264d05c04b
Rebecca Weiss <rebeccaw00@gmail.com>
2026-03-05 17:26:51 +0100

    Adjust GPU usage per agent

--- src/tuning/sweep_box2d_jax.py
+++ src/tuning/sweep_box2d_jax.py
@@ -11,7 +11,7 @@ Usage:
 import os
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-os.environ["JAX_PLATFORMS"] = "cpu"
+os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"] = "0.30"
 
 import argparse
 import torch
