1d05d0da0890465b1df3428ff70eb84cd8d69d34
Rebecca Weiss <rebeccaw00@gmail.com>
2026-03-10 20:19:49 +0100

    Reduce partition on GPU for parallelization

--- src/tuning/sweep_continuous_minigrid_jax.py
+++ src/tuning/sweep_continuous_minigrid_jax.py
@@ -11,7 +11,7 @@ Usage:
 import os
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"] = "0.30"
+os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"] = "0.20"
 
 import argparse
 import torch
