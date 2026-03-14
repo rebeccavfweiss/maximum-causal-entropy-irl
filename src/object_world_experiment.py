6fe33ef69bf7b9a2ca2f96103d9dfd62043fb211
Rebecca Weiss <rebeccaw00@gmail.com>
2026-03-14 15:50:04 +0100

    Remove evaluation environment test as not applicable for tabular agents

--- src/object_world_experiment.py
+++ src/object_world_experiment.py
@@ -58,7 +58,7 @@ def create_config_learner():
 
 def run_experiment(args):
     np.random.seed()
-    n_trajectories, run, objects_train, objects_eval = args
+    n_trajectories, run, objects_train = args
     show = False
     store = False
     experiment_name = "object-world"
@@ -240,39 +240,6 @@ def run_experiment(args):
         }
     )
 
-    # Evaluate on a separate environment with different object placement
-    env_new = create_objectworld_env(
-        gamma=gamma,
-        T=T,
-        grid_size=grid_size,
-        random_start=random_start,
-        continuous=continuous,
-        n_objects=n_objects,
-        objects=objects_eval,
-    )
-
-    reward_demonstrator_new = env_new.compute_true_reward_for_agent(
-        demo, n_trajectories, T
-    )
-    reward_expectation_new = env_new.compute_true_reward_for_agent(
-        agent_expectation, n_trajectories, T
-    )
-    reward_variance_new = env_new.compute_true_reward_for_agent(
-        agent_variance, n_trajectories, T
-    )
-
-    wandb.log(
-        {
-            "reward_demonstrator_new_env": reward_demonstrator_new,
-            "reward_expectation_new_env": reward_expectation_new,
-            "reward_diff_expectation_new_env": reward_expectation_new
-            - reward_demonstrator_new,
-            "reward_variance_new_env": reward_variance_new,
-            "reward_diff_variance_new_env": reward_variance_new
-            - reward_demonstrator_new,
-        }
-    )
-
     wandb.finish()
 
 
@@ -284,10 +251,6 @@ if __name__ == "__main__":
     parser.add_argument(
         "--objects-train", default=None,
         help="Path to JSON with fixed training object config",
-    )
-    parser.add_argument(
-        "--objects-eval", default=None,
-        help="Path to JSON with fixed evaluation object config",
     )
     cli_args = parser.parse_args()
 
@@ -305,21 +268,10 @@ if __name__ == "__main__":
             json.dump(objects_train, f, indent=2)
         print("Saved training object config to experiments/object_world/objects_train.json")
 
-    if cli_args.objects_eval:
-        with open(cli_args.objects_eval, "r") as f:
-            objects_eval = json.load(f)
-    else:
-        # Generate and save an eval config (different from training)
-        seed_env = create_objectworld_env()
-        objects_eval = seed_env.export_objects_config()
-        with open("experiments/object_world/objects_eval.json", "w") as f:
-            json.dump(objects_eval, f, indent=2)
-        print("Saved eval object config to experiments/object_world/objects_eval.json")
-
     tasks = []
     for n_trajectories in [1, 10, 50, 100, 200, 500, 1000, 2500, 5000]:
         for run in range(10):
-            tasks.append((n_trajectories, run, objects_train, objects_eval))
+            tasks.append((n_trajectories, run, objects_train))
 
     with Pool(processes=5) as pool:
         results = pool.map(run_experiment, tasks)
